50ad80138f5c6e08a719421708c1f3c9976d2f2e
ifoatov <izzy88man@gmail.com>
2020-05-26 16:47:49 +0300

    ModalDraggable refactor

--- Magellan/Classes/Transitions/ModalDraggablePresentationController.swift
+++ Magellan/Classes/Transitions/ModalDraggablePresentationController.swift
@@ -8,17 +8,28 @@
 import UIKit
 
 protocol ModalDraggable {
-    var preferredContentHeight: CGFloat { get }
+    var view: UIView! { get }
+    var draggableView: UIView { get }
+    var compactHeight: CGFloat { get }
+    var fullHeight: CGFloat { get }
+    var isBeingDismissed: Bool { get }
+    
+    func dismiss()
+    func viewWillChangeFrame(to frame: CGRect)
 }
 
 class ModalDraggablePresentationViewController: UIPresentationController {
     
+    var modalDraggable: ModalDraggable? {
+        presentedViewController as? ModalDraggable
+    }
+    
     override var frameOfPresentedViewInContainerView: CGRect {
         let bounds = presentingViewController.view.bounds
         
-        if let modalDraggable = presentedViewController as? ModalDraggable {
+        if let modalDraggable = modalDraggable {
             return CGRect(x: 0,
-                          y: bounds.height - modalDraggable.preferredContentHeight,
+                          y: bounds.height - modalDraggable.compactHeight,
                           width: bounds.size.width,
                           height: bounds.size.height)
         }
@@ -31,6 +42,57 @@ class ModalDraggablePresentationViewController: UIPresentationController {
     override func presentationTransitionWillBegin() {
         super.presentationTransitionWillBegin()
         containerView?.addSubview(presentedView!)
+        if let modalDraggable = modalDraggable {
+            let gesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(gesture:)))
+            modalDraggable.draggableView.addGestureRecognizer(gesture)
+        }
     }
+    
+    private var startedHeight: CGFloat = 0
+    @objc private func handlePan(gesture: UIPanGestureRecognizer) {
+        guard let modalDraggable = modalDraggable,
+            let view = modalDraggable.view else {
+            return
+        }
+        
+        switch gesture.state {
+        case .began:
+           startedHeight = gesture.translation(in: view).y
+        case .changed:
+           let newY = gesture.translation(in: view).y
+           let delta = newY - startedHeight
+           startedHeight = newY
+           let newOrigin = CGPoint(x: view.frame.origin.x, y: view.frame.origin.y + delta)
+           if view.bounds.height - newOrigin.y < modalDraggable.compactHeight - 20 {
+            modalDraggable.dismiss()
+               return
+           }
+           let targetFrame = CGRect(x: 0,
+                                    y: view.frame.origin.y + delta,
+                                    width: view.bounds.width,
+                                    height: view.bounds.height)
+           animate(view: view, frame: targetFrame)
+        case .cancelled, .ended:
+            if modalDraggable.isBeingDismissed {
+                return
+            }
+            let futureHeight = view.frame.height - view.frame.origin.y - gesture.velocity(in: view).y
+            let height = (futureHeight - modalDraggable.compactHeight <= modalDraggable.fullHeight - futureHeight)
+                ? modalDraggable.compactHeight
+                : modalDraggable.fullHeight
+            let newOrigin = view.frame.height - height
+            let frame = CGRect(x: 0, y: newOrigin, width: view.bounds.width, height: view.bounds.height)
+            animate(view: view, frame: frame)
+        default:
+            break
+        }
+    }
+       
+    private func animate(view: UIView, frame: CGRect) {
+        modalDraggable?.viewWillChangeFrame(to: frame)
+        UIView.beginAnimations(nil, context: nil)
+        view.frame = frame
+        UIView.commitAnimations()
+   }
     
 }
